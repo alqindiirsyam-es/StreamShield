200098354ee52f6fbd17af27965392fe72fa53a6
alqindiirsyam <alqindi@gmail.com>
2026-01-14 11:29:22 +0700

    update fix bugs

--- StreamShield/Source/SecurityShield.swift
+++ StreamShield/Source/SecurityShield.swift
@@ -28,24 +28,24 @@ public class SecurityShield: NSObject {
         Preference.setAccount(value: apiKey)
         DispatchQueue.global().async {
             do {
-                var id = Preference.getConnectionID()
-                if id.isEmpty {
-                    let sDID = UIDevice.current.identifierForVendor?.uuidString ?? "UNK-DEVICE"
-                    id = String(sDID[sDID.index(sDID.endIndex, offsetBy: -5)...])
-                    Preference.setConnectionID(value: id)
-                }
-                if !API.bnuSDKServiceReady() || API.nGetCLXConnState() == 0 {
-                    let address = getAddressNew(apiKey:Preference.getAccount())
-                    if address.isEmpty {
-                        return
-                    }
-                    let addressConn = address.components(separatedBy: ":")[0]
-                    let port = Int(address.components(separatedBy: ":")[1]) ?? 0
-                    try API.initConnection(sAPIK: apiKey, cbiI: CallBackSS(), sTCPAddr: addressConn, nTCPPort: port, sUserID: id, sStartWH: "09:00")
-                    while (!API.bnuSDKServiceReady() || API.nGetCLXConnState() == 0) {
-                        Thread.sleep(forTimeInterval: 1)
-                    }
-                }
+//                var id = Preference.getConnectionID()
+//                if id.isEmpty {
+//                    let sDID = UIDevice.current.identifierForVendor?.uuidString ?? "UNK-DEVICE"
+//                    id = String(sDID[sDID.index(sDID.endIndex, offsetBy: -5)...])
+//                    Preference.setConnectionID(value: id)
+//                }
+//                if !API.bnuSDKServiceReady() || API.nGetCLXConnState() == 0 {
+//                    let address = getAddressNew(apiKey:Preference.getAccount())
+//                    if address.isEmpty {
+//                        return
+//                    }
+//                    let addressConn = address.components(separatedBy: ":")[0]
+//                    let port = Int(address.components(separatedBy: ":")[1]) ?? 0
+//                    try API.initConnection(sAPIK: apiKey, cbiI: CallBackSS(), sTCPAddr: addressConn, nTCPPort: port, sUserID: id, sStartWH: "09:00")
+//                    while (!API.bnuSDKServiceReady() || API.nGetCLXConnState() == 0) {
+//                        Thread.sleep(forTimeInterval: 1)
+//                    }
+//                }
                 pull()
             } catch {
                 
@@ -205,51 +205,6 @@ public class SecurityShield: NSObject {
         let session = URLSession(configuration: urlConfig, delegate: sessionDelegate, delegateQueue: nil)
         let task = session.dataTask(with: request, completionHandler: completion)
         task.resume()
-    }
-    
-    private static func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 12, weight: .medium), controller: UIViewController) {
-        
-        let toastContainer = UIView(frame: CGRect())
-        toastContainer.backgroundColor = controller.traitCollection.userInterfaceStyle == .dark ? .white.withAlphaComponent(0.6) : UIColor.mainColorSS.withAlphaComponent(0.6)
-        toastContainer.alpha = 0.0
-        toastContainer.layer.cornerRadius = 25;
-        toastContainer.clipsToBounds  =  true
-        
-        let toastLabel = UILabel(frame: CGRect())
-        toastLabel.textColor = controller.traitCollection.userInterfaceStyle == .dark ? .blackDarkModeSS : UIColor.white
-        toastLabel.textAlignment = .center;
-        toastLabel.font = font
-        toastLabel.text = message
-        toastLabel.clipsToBounds  =  true
-        toastLabel.numberOfLines = 0
-        
-        toastContainer.addSubview(toastLabel)
-        controller.view.addSubview(toastContainer)
-        controller.view.bringSubviewToFront(toastContainer)
-        
-        toastLabel.translatesAutoresizingMaskIntoConstraints = false
-        toastContainer.translatesAutoresizingMaskIntoConstraints = false
-        
-        let a1 = NSLayoutConstraint(item: toastLabel, attribute: .leading, relatedBy: .equal, toItem: toastContainer, attribute: .leading, multiplier: 1, constant: 15)
-        let a2 = NSLayoutConstraint(item: toastLabel, attribute: .trailing, relatedBy: .equal, toItem: toastContainer, attribute: .trailing, multiplier: 1, constant: -15)
-        let a3 = NSLayoutConstraint(item: toastLabel, attribute: .bottom, relatedBy: .equal, toItem: toastContainer, attribute: .bottom, multiplier: 1, constant: -15)
-        let a4 = NSLayoutConstraint(item: toastLabel, attribute: .top, relatedBy: .equal, toItem: toastContainer, attribute: .top, multiplier: 1, constant: 15)
-        toastContainer.addConstraints([a1, a2, a3, a4])
-        
-        let c1 = NSLayoutConstraint(item: toastContainer, attribute: .leading, relatedBy: .equal, toItem: controller.view, attribute: .leading, multiplier: 1, constant: 65)
-        let c2 = NSLayoutConstraint(item: toastContainer, attribute: .trailing, relatedBy: .equal, toItem: controller.view, attribute: .trailing, multiplier: 1, constant: -65)
-        let c3 = NSLayoutConstraint(item: toastContainer, attribute: .bottom, relatedBy: .equal, toItem: controller.view, attribute: .bottom, multiplier: 1, constant: -75)
-        controller.view.addConstraints([c1, c2, c3])
-        
-        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
-            toastContainer.alpha = 1.0
-        }, completion: { _ in
-            UIView.animate(withDuration: 0.5, delay: 1.5, options: .curveEaseOut, animations: {
-                toastContainer.alpha = 0.0
-            }, completion: {_ in
-                toastContainer.removeFromSuperview()
-            })
-        })
     }
 }
 
